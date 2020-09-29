1576ab186ce6034565f4886482327e7b59be5603
amojagta <amojagta@AMOJAGTA-3D084.partnet.cisco.com>
2020-09-29 15:33:42 +0530

    Sauce Lab demo

---
+++ SeleniumJavaFramework/src/test/java/demo/SauceLabsDemo.java
@@ -0,0 +1,38 @@
+package demo;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SauceLabsDemo {
+	private static WebDriver driver = null;
+//	public static final String username = "amol306";
+//	public static final String accesskey = "8850d60d-6e50-4c30-abae-22b2fe0e478d";
+//	public static final String URL = "https://" + username + ":" + accesskey + "@ondemand.saucelabs.com:443/wd/hub";
+
+	public static void main(String[] args) {
+		SauceLabsTest();
+		driver.quit();
+	}
+
+	public static void SauceLabsTest() {
+		WebDriverManager.chromedriver().setup();
+
+		// DesiredCapabilities cap = DesiredCapabilities.chrome();
+		// cap.setCapability("platform", "windows 10");
+		// cap.setCapability("version", "latest");
+
+		driver = new ChromeDriver();
+		// driver = new RemoteWebDriver(new java.net.URL(URL), cap);
+
+		driver.get("https://google.com");
+		System.out.println("Title : " + driver.getTitle());
+		driver.findElement(By.name("q")).sendKeys("SDET");
+		driver.findElement(By.xpath("(//input[@name='btnK'])[2]")).sendKeys(Keys.RETURN);
+		driver.close();
+		System.out.println("Test completed!");
+	}
+
+}
